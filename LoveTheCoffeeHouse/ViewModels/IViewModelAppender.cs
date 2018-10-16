a82b1d0b2b424f541e23388cbdf7e19ae11d94a0
SapphireGirl <justinealires@outlook.com>
2018-10-16 08:59:04 -0500

    Added AdventureWorks LT Database to start testing

---
+++ LoveTheCoffeeHouse/ViewModels/IViewModelAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Net.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace LoveTheCoffeeHouse.ViewModels
+{
+    //public interface IViewModelAppender
+    //{
+    //    Task Append(HandleRequestContext<IViewModelAuthorization> handler, dynamic viewModel);
+    //}
+
+    //public interface IViewModelAuthorization : HTTPContext
+    //    //: AuthenticationSchemeOptions
+    //{
+    //}
+}
---
+++ src/Data/RDS/SQL/JA.MS.Data.SQL/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JA.MS.Data.SQL
+{
+    public interface IRepository
+    {
+
+
+    }
+
+    public class IEntity
+    {
+        public int Id;
+    }
+
+    public interface IRepository<T> where T : IEntity
+    {
+
+    }
+}
